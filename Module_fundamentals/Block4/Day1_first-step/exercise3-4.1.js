97677cb7280642cf7057920dc67c8ba101da4654
Débora Serra <debora.r.serra@gmail.com>
2021-11-25 10:25:46 -0300

    Add day4.1 exercises

---
+++ Module_fundamentals/Block4/Day1_first-step/exercise3-4.1.js
@@ -0,0 +1,9 @@
+const score = 85;
+
+if (score >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+} else if (score < 80 && score >= 60) {
+    console.log("Você está na lista de espera.")
+} else {
+    console.log("Você foi reprovado")
+}
