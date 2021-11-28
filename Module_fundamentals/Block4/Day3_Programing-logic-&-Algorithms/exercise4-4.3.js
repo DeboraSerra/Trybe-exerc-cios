ec7b577d8be90ce2bdec6729ba5a0889567cccd4
Débora Serra <debora.r.serra@gmail.com>
2021-11-28 13:18:53 -0300

    Add the exercise 4 from day 4.3

--- Module_fundamentals/Block4/Day3_Programing-logic-&-Algorithms/exercise4-4.3.js
+++ Module_fundamentals/Block4/Day3_Programing-logic-&-Algorithms/exercise4-4.3.js
@@ -1,18 +1,22 @@
 let n = 5;
 let line = '';
-let position = n;
 
-for (let i = 0; i <= n; i += 1){
+let midPosition = (n + 1) / 2;
+let left = midPosition;
+let right = midPosition;
+
+for (let i = 0; i <= midPosition; i += 1){
   for (let x = 0; x <= n; x += 1){
-    if (x < position) {
-      line += ' ';
-    } else {
+    if (x > left && x < right) {
       line += '*';
+    } else {
+      line += ' ';
     }
   }
   console.log(line);
   line = '';
-  position -= 1;
+  right += 1;
+  left -= 1;
 }
 
 
