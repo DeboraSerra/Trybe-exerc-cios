fc8f88e0a65cdae8970727467d66b8c46f01e494
Débora Serra <debora.r.serra@gmail.com>
2021-11-28 10:40:48 -0300

    Add the bonus exercises from day 4.2

---
+++ Module_fundamentals/Block4/Day2_Array-&-loop-for/bonusExercise3-4.2.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
+
+for (i = 0; i < numbers.length; i += 1){
+  if (numbers[i + 1] == undefined) {
+    newNumbers[i] = numbers[i] * 2;
+  } else {
+    newNumbers.push(numbers[i] * numbers[i + 1]);
+  }
+}
+
+
+console.log(newNumbers);
