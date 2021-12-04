676fe8b0e0047b61a2ce9d0e8e087edaa55f127e
Débora Serra <debora.r.serra@gmail.com>
2021-12-04 12:03:25 -0300

    update the exexercise1 from day 5.2

--- Module_fundamentals/Block5/Day2_working-with-elements/Fixing-exercises/script1.js
+++ Module_fundamentals/Block5/Day2_working-with-elements/Fixing-exercises/script1.js
@@ -1,25 +1,25 @@
-console.log(document.querySelector("#elementoOndeVoceEsta"));
+// console.log(document.querySelector("#elementoOndeVoceEsta"));
 
-console.log(
-  (document.querySelector("#elementoOndeVoceEsta").parentNode.style.color =
-    "blue")
-);
+// console.log(
+//   (document.querySelector("#elementoOndeVoceEsta").parentNode.style.color =
+//     "blue")
+// );
 console.log(
   (document.querySelector("#primeiroFilhoDoFilho").innerText =
     "Esse é o primeiro filho do filho.")
 );
-console.log(
-  (document.querySelector("#pai").firstChild.innerText =
-    "Primeiro filho a partir de pai")
-);
-console.log(
-  (document.querySelector("#elementoOndeVoceEsta").previousSibling.innerText =
-    "Primeiro filho a partir de elementoOndeVoceEsta")
-);
-console.log(
-  (document.querySelector("#elementoOndeVoceEsta").nextSibling.innerText =
-    "Esse é o atenção")
-);
+// console.log(
+//   (document.querySelector("#pai").firstChild.innerText =
+//     "Primeiro filho a partir de pai")
+// );
+// console.log(
+//   (document.querySelector("#elementoOndeVoceEsta").previousSibling.innerText =
+//     "Primeiro filho a partir de elementoOndeVoceEsta")
+// );
+// console.log(
+//   (document.querySelector("#elementoOndeVoceEsta").nextSibling.innerText =
+//     "Esse é o atenção")
+// );
 console.log(
   (document.querySelector(
     "#elementoOndeVoceEsta"
@@ -41,47 +41,46 @@ for (let i = 0; i < list.length; i += 1){
   let createListItem = document.createElement('li');
   createListItem.innerText = listItem;
   parent.appendChild(createListItem);
+  parent.removeChild(createListItem);
 }
+
 
 let text = "I'm a <strong>newborn</strong> sibling to elementoOndeVoceEsta";
 let createTag = document.createElement('p');
 createTag.innerHTML = text;
 parent.appendChild(createTag);
+parent.removeChild(createTag);
 
 let elementWhereIAm = document.querySelector("#elementoOndeVoceEsta");
 let newText = "I'm a new kid to elementoOndeVoceEsta";
 let newTag = document.createElement('h3');
 newTag.innerText = newText;
 elementWhereIAm.appendChild(newTag)
+elementWhereIAm.removeChild(newTag);
 
 let firstChildOfChild = document.querySelector("#primeiroFilhoDoFilho");
 let otherText = "I'm a kid to primeiroFilhoDoFilho";
 let otherTag = document.createElement('h4');
 otherTag.innerText = otherText;
 firstChildOfChild.appendChild(otherTag);
+firstChildOfChild.removeChild(otherTag);
 
-document.querySelector("#primeiroFilhoDoFilho h4").className = "filhoDoPrimeiroFilhoDoFilho";
+let atention = parent.firstChild.nextSibling.nextElementSibling.nextElementSibling.nextSibling;
+parent.removeChild(atention);
 
-console.log(document.querySelector('.filhoDoPrimeiroFilhoDoFilho').parentNode.parentElement.nextSibling.nextElementSibling.innerText += '. I\'m the third child');
 
-let father = document.querySelector("#pai");
 
 let firstChild = document.querySelector('#primeiroFilho');
-father.removeChild(firstChild);
+parent.removeChild(firstChild);
 
 let secondChildOfChild = document.querySelector('#segundoEUltimoFilhoDoFilho');
 document.querySelector('#elementoOndeVoceEsta').removeChild(secondChildOfChild);
 
 
 let thirdKid = document.querySelector('#terceiroFilho');
-father.removeChild(thirdKid);
+parent.removeChild(thirdKid);
 
 let fourthKid = document.querySelector("#quartoEUltimoFilho");
-father.removeChild(fourthKid);
+parent.removeChild(fourthKid);
 
-let listOfItems = document.querySelectorAll('li');
-
-for (let i = 0; i < listOfItems.length; i += 1) {
-  father.removeChild(listOfItems[i]);
-}
 
