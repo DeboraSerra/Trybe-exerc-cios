2dcf75bab54fdaf02760c2678b1b88b507cffcde
Débora Serra <debora.r.serra@gmail.com>
2022-01-13 11:52:35 -0300

    Refactor project trybewarts

--- Module1_fundaments/Block6/Project-Trybewarts/script.js
+++ Module1_fundaments/Block6/Project-Trybewarts/script.js
@@ -7,105 +7,94 @@ const textArea = document.querySelector('#textArea');
 const counter = document.querySelector('#counter');
 const form = document.querySelector('#evaluation-form');
 const select = document.querySelector('#house');
-const formContent = document.querySelector('.form-content');
 const sandwichIcon = document.querySelector('.icon');
 const loginInput = document.querySelector(".trybewarts-login");
 
-sandwichIcon.addEventListener('click', () => {
-  loginInput.classList.toggle('login-active')
-})
+sandwichIcon.addEventListener('click', () => loginInput.classList.toggle('login-active'));
 
-function checkLogIn() {
-  console.log(typeof loginPassword.value);
-  console.log(loginEmail.value);
-  if (
-    loginEmail.value === "tryber@teste.com" &&
-    loginPassword.value === "123456"
-  ) {
-    alert("Hi, Tryber!");
-  } else {
+const checkLogIn = () => {
+  (loginEmail.value === "tryber@teste.com" && loginPassword.value === "123456") ? 
+    alert("Hi, Tryber!") :
     alert("Wrong e-mail or password");
-  }
 }
 
 loginButton.addEventListener("click", checkLogIn);
 
-function checkAgreement() {
-  if (agree.checked) {
-    submitButton.removeAttribute('disabled');
-  } else {
-    submitButton.setAttribute('disabled', 'disabled')
-  }
+const checkAgreement = () => {
+  (agree.checked) ? submitButton.removeAttribute('disabled') : submitButton.setAttribute('disabled', 'disabled');
 }
 
 agree.addEventListener('click', checkAgreement);
 
-function countDown() {
+const countDown = () => {
   const characters = textArea.value.length;
   let myCounter = 500;
-  if (textArea.value.length !== 0) {
-    counter.innerHTML = myCounter - characters;
-  } else {
-    counter.innerHTML = 500;
-  }
+  (textArea.value.length !== 0) ? counter.innerHTML = myCounter - characters : counter.innerHTML = 500;
 }
 
 textArea.addEventListener('keyup', countDown);
 
-function createFullName() {
+const formContent = document.createElement('section');
+
+const createFullName = () => {
   const name = document.querySelector("#input-name");
   const lastName = document.querySelector("#last-name");
   const fullName = document.createElement('p');
   fullName.innerText = `Nome: ${name.value} ${lastName.value}`;
   formContent.appendChild(fullName);
+  return true;
 }
 
-function createEmail() {
+const createEmail = () => {
   const email = document.querySelector("#input-email");
   const myEmail = document.createElement('p');
   myEmail.innerText = `Email: ${email.value}`;
   formContent.appendChild(myEmail);
+  return true;
 }
 
-function createHouse() {
+const createHouse = () => {
   const selectedHouse = select.options[select.selectedIndex];
   const house = document.createElement('p');
   house.innerText = `Casa: ${selectedHouse.value}`;
   formContent.appendChild(house);
+  return true;
 }
 
-function createFamily() {
+const createFamily = () => {
   const selectedFamily = document.querySelector('input[name=family]:checked');
   const family = document.createElement('p');
   family.innerText = `Família: ${selectedFamily.value}`;
   formContent.appendChild(family);
+  return true;
 }
 
-function createSubjects() {
+const createSubjects = () => {
   const selectedSubjects = document.querySelectorAll(".subject:checked");
   const subjects = [];
-  for (let i = 0; i < selectedSubjects.length; i += 1) {
-    subjects.push(selectedSubjects[i].value);
-  }
+  selectedSubjects.forEach((item) => subjects.push(item.value));
   const mySubjects = document.createElement('p');
   mySubjects.innerText = `Matérias: ${subjects.join(', ')}`;
   formContent.appendChild(mySubjects);
+  return true;
 }
 
-function createRating() {
+const createRating = () => {
   const selectedRate = document.querySelector('input[name=rate]:checked');
   const rate = document.createElement('p');
   rate.innerText = `Avaliação: ${selectedRate.value}`;
   formContent.appendChild(rate);
+  return true;
 }
 
-function createComment() {
+const createComment = () => {
   const myComment = document.createElement('p');
   myComment.innerHTML = `Observações: ${textArea.value}`;
   formContent.appendChild(myComment);
+  return true;
 }
 
-function submit(e) {
+const submit = (e) => {
   e.preventDefault();
   createFullName();
   createEmail();
@@ -115,7 +104,7 @@ function submit(e) {
   createRating();
   createComment();
   form.innerHTML = '';
-  form.style.width = '1px';
+  form.appendChild(formContent);
 }
 
 
